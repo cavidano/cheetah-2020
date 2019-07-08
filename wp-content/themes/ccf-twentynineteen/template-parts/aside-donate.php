912a509911410716f491d025fcc2ca88933cd6ae
Carl Avidano <cavidano@gmail.com>
2019-07-08 07:48:09 -0400

    made donate sidebar acf

--- wp-content/themes/ccf-twentynineteen/template-parts/aside-donate.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/aside-donate.php
@@ -1,36 +1,59 @@
-<div class="d-none d-xl-block">
+<?php if (have_rows('donate_panel', 'options')):
 
-    <div class="row matrix-border">
+    while (have_rows('donate_panel', 'options')): the_row();
 
-        <div class="col-12">
-            <div class="card bg-dark d-block">
-            <img class="card-img opacity-40" src="<?php echo get_template_directory_uri(); ?>/images/donate-aside.jpg" alt="Placeholder">
-                <div class="card-img-overlay d-flex">
-                    <div class="align-self-center w-100">
-                        <div class="text-white text-center text-shadow">
-                            <p class="h4">
-                               <span class="d-block">Join the race</span>
-                               <a href="#1" class="text-primary">#SaveTheCheetah</a>
-                            </p>
+    $headline = get_sub_field('headline');
+    $link = get_sub_field('link');
+
+    $image_sidebar = get_sub_field('image_sidebar');
+
+    ?>
+
+    <div class="d-none d-xl-block">
+
+        <div class="row matrix-border">
+
+            <div class="col-12">
+
+                <div class="card bg-dark d-block">
+
+                    <?php if ($image_sidebar): ?>
+                        <img class="card-img opacity-40" src="<?php echo $image_sidebar['url']; ?>" alt="<?php echo $image_sidebar['alt']; ?>">
+                    <?php else : ?>
+                        <img class="card-img opacity-40" src="<?php echo get_template_directory_uri(); ?>/images/donate-aside.jpg" alt="Placeholder">
+                    <?php endif; ?>
+
+                    
+                    <div class="card-img-overlay d-flex">
+                        <div class="align-self-center w-100">
+                            <div class="text-white text-center text-shadow">
+                                <p class="h4">
+                                <span class="d-block"><?php echo $headline; ?></span>
+                                
+                                <a class="text-primary" href="<?php echo $link['url']; ?>" <?php if($link['target']) : ?>target="<?php echo $link['target'] ?>"<?php endif; ?>><?php echo $link['title']; ?></a>
+                                </p>
+                            </div>
                         </div>
                     </div>
+                    <!-- .card-img-overlay -->
                 </div>
+                <!-- .card -->
             </div>
-            <!-- .card -->
-        </div>
-        <!-- .col -->
+            <!-- .col -->
 
-        <div class="col-xxl-6">
-            <a class="btn btn-block btn-primary" href="/donate" title="Donate">Donate</a>
+            <div class="col-xxl-6">
+                <a class="btn btn-block btn-primary" href="<?php echo home_url(); ?>/donate" title="Donate">Donate</a>
+            </div>
+            <!-- .col -->
+            
+            <div class="col-xxl-6">
+                <a class="btn btn-block btn-primary" href="<?php echo home_url(); ?>/donate/sponsor/" title="Sponsor">Sponsor</a>
+            </div>
+            <!-- .col -->
+
         </div>
-        <!-- .col -->
-        
-        <div class="col-xxl-6">
-            <a class="btn btn-block btn-primary" href="/donate/sponsor/" title="Sponsor">Sponsor</a>
-        </div>
-        <!-- .col -->
+        <!-- .matrix-border -->
 
     </div>
-    <!-- .matrix-border -->
 
-</div>
+<?php endwhile; endif; /* donate_panel */ ?>
--- wp-content/themes/ccf-twentynineteen/template-parts/donate-panel.php
+++ wp-content/themes/ccf-twentynineteen/template-parts/donate-panel.php
@@ -1,63 +1,62 @@
 <?php if (have_rows('donate_panel', 'options')):
 
-while (have_rows('donate_panel', 'options')): the_row();
-    
-// vars
-$headline = get_sub_field('headline');
-$link = get_sub_field('link');
+  while (have_rows('donate_panel', 'options')): the_row();
 
-$image_full = get_sub_field('image_full');
+  $headline = get_sub_field('headline');
+  $link = get_sub_field('link');
 
-?>
+  $image_full = get_sub_field('image_full');
 
-<section class="featured-panel responsive-lg">
+  ?>
 
-      <div class="card bg-dark">
+  <section class="featured-panel responsive-lg">
 
-        <?php if ($image_full): ?>
-            <img class="card-img opacity-20 show-on-mobile" src="<?php echo $image_full['url']; ?>" alt="<?php echo $image_full['alt']; ?>">
-        <?php else : ?>
-            <img class="card-img opacity-20 show-on-mobile" src="<?php echo get_template_directory_uri(); ?>/images/donate.jpg" alt="Placeholder">
-        <?php endif; ?>
+    <div class="card bg-dark">
 
-        <div class="card-img-overlay d-flex px-0">
+      <?php if ($image_full): ?>
+          <img class="card-img opacity-20 show-on-mobile" src="<?php echo $image_full['url']; ?>" alt="<?php echo $image_full['alt']; ?>">
+      <?php else : ?>
+          <img class="card-img opacity-20 show-on-mobile" src="<?php echo get_template_directory_uri(); ?>/images/donate.jpg" alt="Placeholder">
+      <?php endif; ?>
 
-          <div class="container-fluid align-self-center">
-          
-            <div class="narrow py-3 text-white text-center">
+      <div class="card-img-overlay d-flex px-0">
 
-              <h2 class="h1 text-shadow mb-2">
-                  <?php echo $headline; ?>
-                  <a class="text-primary" href="<?php echo $link['url']; ?>" <?php if($link['target']) : ?>target="<?php echo $link['target'] ?>"<?php endif; ?>><?php echo $link['title']; ?></a>
-              </h2>
+        <div class="container-fluid align-self-center">
+        
+          <div class="narrow py-3 text-white text-center">
 
-              <div class="row matrix-border">
+            <h2 class="h1 text-shadow mb-2">
+                <?php echo $headline; ?>
+                <a class="text-primary" href="<?php echo $link['url']; ?>" <?php if($link['target']) : ?>target="<?php echo $link['target'] ?>"<?php endif; ?>><?php echo $link['title']; ?></a>
+            </h2>
 
-                <div class="col-md-6">
-                  <a class="btn btn-lg btn-block btn-primary" href="/donate" title="Donate">Donate</a>
-                </div>
-                <!-- .col -->
+            <div class="row matrix-border">
 
-                <div class="col-md-6">
-                  <a class="btn btn-lg btn-block btn-primary" href="/donate/sponsor/" title="Sponsor">Sponsor</a>
-                </div>
-                <!-- .col -->
-
+              <div class="col-md-6">
+                <a class="btn btn-lg btn-block btn-primary" href="<?php echo home_url(); ?>/donate" title="Donate">Donate</a>
               </div>
-              <!-- .matrix-border -->
+              <!-- .col -->
 
-          </div>
-          <!-- .narrow -->
+              <div class="col-md-6">
+                <a class="btn btn-lg btn-block btn-primary" href="<?php echo home_url(); ?>/donate/sponsor/" title="Sponsor">Sponsor</a>
+              </div>
+              <!-- .col -->
 
-          </div>
-          <!-- .container -->
+            </div>
+            <!-- .matrix-border -->
 
         </div>
-        <!-- .card-img-overlay -->
+        <!-- .narrow -->
+
+        </div>
+        <!-- .container -->
 
       </div>
-      <!-- .card -->
+      <!-- .card-img-overlay -->
 
-    </section>
+    </div>
+    <!-- .card -->
 
-	<?php endwhile; endif; /* donate_panel */ ?>
+  </section>
+
+<?php endwhile; endif; /* donate_panel */ ?>
