148a0953cbf1788fd517b8dcdee5e43ff2d8feab
Carl Avidano <cavidano@gmail.com>
2019-08-05 08:54:23 -0400

    removed header scripts

--- wp-content/themes/ccf-twentynineteen/header.php
+++ wp-content/themes/ccf-twentynineteen/header.php
@@ -20,24 +20,6 @@ $inside_news = strpos($url, 'news');
     
     <?php wp_head(); ?>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-491379-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-491379-2');
-    </script>
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WC5Q9NX');</script>
-    <!-- End Google Tag Manager -->
-
 </head>
 
 <body <?php body_class(); ?>>
